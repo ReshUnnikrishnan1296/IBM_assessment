6f14ea7c86f8714ff6f745d3f88aa7ac227100c5
ReshUnnikrishnan1296 <rs415638@dal.ca>
2022-03-25 16:06:48 -0300

    product-details css

--- IBM-assessment/src/app/app-routing.module.ts
+++ IBM-assessment/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'product_details', component: ProductDetailsComponent }
+  { path: 'product_details', component: ProductDetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
